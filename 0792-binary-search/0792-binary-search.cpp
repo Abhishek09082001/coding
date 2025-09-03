31476407f46d4b26a8279e1930320fa1f195588b
Abhishek09082001 <93652601+Abhishek09082001@users.noreply.github.com>
2025-09-03 23:57:54 +0530

    Time: 0 ms (100%), Space: 31.3 MB (80.01%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -6,15 +6,11 @@ public:
        while(l<=r){
         int mid=l+(r-l)/2;
             if(nums[mid] == target)
-                {
-                return mid;
-                }
+                {return mid;}
             else if(nums[mid] < target)
-            {
-                l=mid+1;
-            }
+            {l=mid+1;}
                 else{r=mid-1;}
-    }
+            }
             return -1;
     }
 };
